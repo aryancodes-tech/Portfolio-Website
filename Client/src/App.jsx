2b6f297026f1169fc07cee9a3c13d76517974657
aryancodes-tech <aryangupta8291@gmail.com>
2026-05-07 05:49:48 +0530

    add redirection

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,24 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import { Analytics } from "@vercel/analytics/react"
-// import { RedirectToResume } from './components/RedirectToResume'
 import { useEffect } from 'react'
+import { DSA_EXTERNAL_URL } from './constants/urls'
 
+/** Sends the browser to the public resume PDF on Google Drive. */
 function RedirectToResume() {
   useEffect(() => {
     window.location.href = "https://drive.google.com/file/d/1fxvjQcduLKS4ewpOm_j__fUBywM902mD/view?usp=sharing";
+  }, []);
+  return null;
+}
+
+/** Sends the browser to the DSA site (dsa.aryancodes.tech). */
+function RedirectToDsa() {
+  useEffect(() => {
+    if (DSA_EXTERNAL_URL.length > 0) {
+      window.location.replace(DSA_EXTERNAL_URL)
+    }
   }, []);
   return null;
 }
@@ -24,6 +35,7 @@ function App() {
             <RedirectToResume />
           } 
         />
+        <Route path="/dsa" element={<RedirectToDsa />} />
       </Routes>
     </BrowserRouter>
     <Analytics />
---
+++ Client/src/constants/urls.js
@@ -0,0 +1,5 @@
+/**
+ * Absolute URL for the DSA microsite (subdomain).
+ * @type {string}
+ */
+export const DSA_EXTERNAL_URL = 'https://dsa.aryancodes.tech'
